889d292db5797d19d4deccee9f0ae88715bd8e4f
Dave <davidairborne@gmail.com>
2019-02-15 11:06:26 -0800

    drum machine

--- src/ffmpeg/drummachinenote/drummachinenote.ts
+++ src/ffmpeg/drummachinenote/drummachinenote.ts
@@ -1,34 +1,42 @@
 import { LightningElement, api } from 'lwc';
-import { TickRange } from 'store/tick';
+import { TickRange, Tick } from 'store/tick';
 import { PianoKey } from 'util/sound';
 import { generateId } from 'util/uniqueid';
 import { MidiNoteCreatedEvent, midiNoteCreatedEvent } from 'event/midinotecreatedevent';
+import { Tempo } from 'store/project';
+import { Rect } from 'interactjs';
+import { Color } from 'util/color';
 import { MidiNoteDeletedEvent, midiNoteDeletedEvent } from 'event/midinotedeletedevent';
 
 export default class DrumMachineTick extends LightningElement {
     @api range: TickRange;
+    @api tempo: Tempo;
+    @api parentVisibleRange: TickRange;
+    @api parentRect: Rect;
+    @api quanitizeResolution: Tick;
     @api pianoKey: PianoKey;
     @api noteId?: string;
     @api isPlaying: boolean;
-    get divClass(): string {
-        const classNames: string[]= [];
-        if (this.noteId) {
-            classNames.push('active');
-        }
+
+    get noteColor() {
         if (this.isPlaying) {
-            classNames.push('playing');
+            return new Color(47, 97, 147);
         }
 
-        return classNames.join(' ');
+        if (this.noteId) {
+            return new Color(163, 163, 163);
+        }
+        return new Color(61, 61, 61);
     }
 
-    onDivClick() {
+    onMidiNoteClick(evt) {
         const { noteId } = this;
         if (!noteId) {
             const createMidiEvent: MidiNoteCreatedEvent = midiNoteCreatedEvent(this.pianoKey, generateId(), this.range);
             this.dispatchEvent(createMidiEvent);
             return;
         }
+
         const deleteMidiEvent: MidiNoteDeletedEvent = midiNoteDeletedEvent(this.pianoKey, noteId);
         this.dispatchEvent(deleteMidiEvent);
     }
--- src/ffmpeg/midinote/midinote.ts
+++ src/ffmpeg/midinote/midinote.ts
@@ -5,27 +5,59 @@ import { Rect } from 'util/geometry';
 import { Tempo } from 'store/project';
 import interactjs, { Interactable } from 'interactjs';
 import { MidiNoteRangeChangedEvent, midiNoteRangeChangedEvent } from 'event/midinoterangechangedevent';
+import { MidiNoteDeletedEvent, midiNoteDeletedEvent } from 'event/midinotedeletedevent';
+import { Color } from 'util/color';
 
 
 export default class MidiNoteElement extends LightningElement {
     @api pianoKey: PianoKey;
-    @api noteId: string;
+    @api noteId?: string;
     @api range: TickRange;
     @api parentVisibleRange: TickRange;
     @api parentRect: Rect;
     @api quanitizeResolution: Tick;
     @api tempo: Tempo;
+    @api color: Color | null = null;
+    @api noDrag: boolean = false;
 
     interactable: Interactable | null = null;
 
+    get buttonStyle() {
+        let { color } = this;
+        if (!color) {
+            color = new Color(88, 197, 253);
+        }
+        return `background: ${color.rgb()}`;
+    }
+
+    onButtonKeyUp(evt: KeyboardEvent) {
+        switch (evt.which) {
+            case 8:
+                // ESC
+                const { noteId } = this;
+                if (noteId) {
+                    const deleteEvent: MidiNoteDeletedEvent = midiNoteDeletedEvent(this.pianoKey, noteId);
+                    this.dispatchEvent(deleteEvent);
+                }
+                break;
+        }
+    }
+
     renderedCallback() {
         if (!this.interactable) {
+            // Even if "noDrag" prop is false,
+            // We have to hookup the interactable
+            // because we need to capture the event before it reaches the audio window
             this.interactable = interactjs(this.template.querySelector('.container')).draggable({
                 onmove: (evt) => {
+                    const { noteId, noDrag } = this;
+                    if (!noteId || noDrag === true) {
+                        return;
+                    }
                     const delta = pixelToTick(this.parentRect, this.parentVisibleRange, evt.dx);
                     const event: MidiNoteRangeChangedEvent = midiNoteRangeChangedEvent({
                         key: this.pianoKey,
-                        noteId: this.noteId,
+                        noteId,
                         currentRange: this.range,
                         durationDelta: tickZero,
                         quanitizeResolution: this.quanitizeResolution,
